63c65bcc2fa192a70404eda69c98ff9130c8fc7f
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-03-26 18:04:51 +0530

    Update gcp-firewall-terra.tf

--- gcp-firewall-terra.tf
+++ gcp-firewall-terra.tf
@@ -1,18 +1,3 @@
-# Declare the provider (GCP)
-# provider "google" {
-#   project =  "jagriti-411012"
-#   credentials = "${file("C:/Users/JAGRITI/Downloads/jagriti-411012-0efe7430eaf6.json")}"
-#   region  = "us-central1"
-#   # zone = "us-central1-a"
-#   #project = "terraform-project1-417611"
-# }
-# provider "google" {
-#   project     = "jagriti-411012"
-#   credentials = jsondecode(file("C:/Users/JAGRITI/Downloads/jagriti-411012-0efe7430eaf6.json"))
-#   region      = "us-central1"
-# }
-
-# Define the firewall rule to allow traffic on ports 88, 443, and 22
 resource "google_compute_firewall" "allow_ports" {
   name    = "ter-firewall"
   network = "default"
@@ -25,7 +10,6 @@ resource "google_compute_firewall" "allow_ports" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-# Define the VM instance
 resource "google_compute_instance" "vm_giit" {
   name         = "terraform-project"
   machine_type = "n1-standard-1"
