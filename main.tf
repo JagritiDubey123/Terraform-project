3dac9f4ff225d6059968707677b0fa28a5f44580
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-03-26 18:07:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-# main.tf
 provider "google" {
   project =  "jagriti-411012"
   credentials = "${file("C:/Users/JAGRITI/Downloads/jagriti-411012-f2cd08035bac.json")}"
-  #region  = ""
-  # zone = "us-central1-a"
-  #project = "terraform-project1-417611"
+ 
 }
-# Declare the provider (GCP)
-# provider "google" {
-#   project = "jagriti-411012"
-#   region  = "us-central1"
-# }
+
 
 # Define the VM instance
 resource "google_compute_instance" "vm_instance" {
@@ -30,17 +23,6 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-# resource "google_project" "terraform-project" {
-#   name       = "terraform-project"
-#   project_id = "jagriti-terraform-123"  # Specify a unique project ID for your new project
-#   org_id     = null  # Set org_id to null for a standalone project
-# }
-
-
-# resource "google_project" "giit" {
-#   name       = "terraform-giiit"
-#   project_id = "terraform-giit-12345"
-# }
 
 
 
@@ -52,20 +34,3 @@ resource "google_compute_instance" "vm_instance" {
 
 
 
-
- # machine_type = "f1-micro"
-  # zone = "us-central1-a"
-  # allow_stopping_for_updates = true
-
-
-  # boot_disk{
-  #   initialize_params{
-  #     image = "debian-cloud/debian-10"
-  #   }
-  # }
-  # network_interface {
-  #   network = "default"
-  #   access_config {
-  #           //necessary even empty
-  #   }
-  # }
