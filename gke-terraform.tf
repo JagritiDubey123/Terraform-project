6a7235937123477e16c3dfb0948ee32e5e334308
Jagriti <129485043+JagritiDubey123@users.noreply.github.com>
2024-04-08 16:43:57 +0530

    Add files via upload

--- gke-terraform.tf
+++ gke-terraform.tf
@@ -1,57 +1,32 @@
 provider "google" {
-  project = "jagriti-411012"
-   credentials = "${file("C:/Users/JAGRITI/Downloads/jagriti-411012-f2cd08035bac.json")}"
-#   region  = "us-central1"
+  project     = "jagriti-411012"
+  credentials = file("C:/Users/JAGRITI/Downloads/jagriti-411012-4c9ab7bf4591.json")
+  #   region  = "us-central1"
 }
-
-
-# Create a GKE cluster
-resource "google_container_cluster" "my_cluster21" {
-  name               = "my-cluster21"
-  location           = "us-central1"
-  remove_default_node_pool = false
-
-  # Set deletion protection to false to allow cluster deletion
+resource "google_container_cluster" "my_cluster" {
+  name     = "giit-gke-cluster"
+  location = "us-central1-a"
   deletion_protection = false
+   remove_default_node_pool = true
+  initial_node_count = 1
 
-  # Network configuration
-  network           = google_compute_network.my_network12.name
-  subnetwork        = google_compute_subnetwork.my_subnet1.name
-
-  # Addon configuration
-  addons_config {
-    network_policy_config {
-      disabled = true
-    }
-  }
-
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-
-  # Node pool with private nodes
-  node_pool {
-    name               = "private-pool"
-    initial_node_count = 1
-    node_locations     = ["us-central1"]
-
-    management {
-      auto_repair  = true
-      auto_upgrade = true
-    }
-
-    # Labels for node pool (optional)
-    node_config {
-      labels = {
-        "environment" = "production"
-      }
-    }
-  }
+  network    = google_compute_network.my_network12.name
+  subnetwork = google_compute_subnetwork.my_subnet1.name
 }
 
-# Create a Google Compute Network
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.my_cluster.name
+  node_count = 1
+
+  node_config {
+     preemptible  = true
+    machine_type = "e2-medium"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+  }
+}
 resource "google_compute_network" "my_network12" {
   name                    = "my-network12"
   auto_create_subnetworks = false
@@ -59,8 +34,8 @@ resource "google_compute_network" "my_network12" {
 
 # Create a Google Compute Subnetwork
 resource "google_compute_subnetwork" "my_subnet1" {
-  name          = "my-subnet1"
-  network       = google_compute_network.my_network12.self_link
+  name          = "giit-subnet1"
+  network       = google_compute_network.my_network12.id
   region        = "us-central1"
   ip_cidr_range = "10.0.0.0/24"
 }
